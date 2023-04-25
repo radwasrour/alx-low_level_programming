a2c04d0e53e0f79686be2973e5221ccf1f6c91ff
radwasrour <radwa.samy1011@gmail.com>
2023-04-25 03:31:53 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:A C program that prints with printf or puts function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of a an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
+}
