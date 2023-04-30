2d9a773274dcfa3382700789b9024bed205fe64b
radwasrour <radwa.samy1011@gmail.com>
2023-04-30 06:54:19 -0700

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
