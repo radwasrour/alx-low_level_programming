891869493f875213da708e302163e64799d0b97b
radwasrour <radwa.samy1011@gmail.com>
2023-04-30 03:14:00 -0700

    b

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar (ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
